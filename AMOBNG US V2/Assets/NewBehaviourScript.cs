1dcf4de51e1e72c6c940bee95805150b5dc875b8
Hxbrs <JChanpog@gmail.com>
2022-10-10 17:04:20 -0700

    THIS IS NEW CODE
    I KNOW HOW TO CODE

---
+++ AMOBNG US V2/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.out.println("among us");
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
